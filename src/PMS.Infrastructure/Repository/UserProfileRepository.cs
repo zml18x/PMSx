c183f30f8bd1e1b59e493f03f3440c78c63fd14f
ZML <zml18v@gmail.com>
2023-07-08 17:05:14 +0200

    Test 2

---
+++ src/PMS.Infrastructure/Repository/UserProfileRepository.cs
@@ -0,0 +1,28 @@
+﻿using PMS.Core.Entities;
+using PMS.Core.Repository;
+
+namespace PMS.Infrastructure.Repository
+{
+    public class UserProfileRepository : IUserProfileRepository
+    {
+        public Task<UserProfile> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(UserProfile userProfile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(UserProfile userProfile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(UserProfile userProfile)
+        {
+            throw new NotImplementedException();
+        }    
+    }
+}
---
+++ src/PMS.Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using PMS.Core.Entities;
+using PMS.Core.Repository;
+
+namespace PMS.Infrastructure.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<User> GetAsyncById(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetAsyncByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
