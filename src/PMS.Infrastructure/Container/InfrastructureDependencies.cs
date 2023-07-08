48eac6bb1a5a6861ddc5c3c091d5c37421373438
ZML <zml18v@gmail.com>
2023-07-08 17:33:53 +0200

    Entity framework added

---
+++ src/PMS.Infrastructure/Container/InfrastructureDependencies.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PMS.Core.Repository;
+using PMS.Infrastructure.Data.Context;
+using PMS.Infrastructure.Repository;
+
+namespace PMS.Infrastructure.Container
+{
+    public static class InfrastructureDependencies
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PmsDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+
+
+            services.AddScoped<IUserRepository,UserRepository>();
+            services.AddScoped<IUserProfileRepository,UserProfileRepository>();
+        }
+    }
+}
---
+++ src/PMS.Infrastructure/Data/Context/PmsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace PMS.Infrastructure.Data.Context
+{
+    public class PmsDbContext : DbContext
+    {
+        public PmsDbContext() { }
+
+        public PmsDbContext(DbContextOptions<PmsDbContext> options) : base(options) { }
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
