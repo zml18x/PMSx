55587a550a59aebaff9cb4627b8441d388b6e99a
ZML <zml18v@gmail.com>
2023-08-01 16:52:58 +0200

    Created PropertyRepository

---
+++ src/PMS.Core/Repository/IPropertyRepository.cs
@@ -0,0 +1,13 @@
+﻿using PMS.Core.Entities;
+
+namespace PMS.Core.Repository
+{
+    public interface IPropertyRepository
+    {
+        public Task<Property> GetByIdAsync(Guid propertyId);
+        public Task<IEnumerable<Property>> GetAllAsync(Guid userId);
+        public Task CreateAsync(Property property);
+        public Task UpdateAsync(Property property);
+        public Task DeleteAsync(Property property);
+    }
+}
---
+++ src/PMS.Infrastructure/Repository/PropertyRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMS.Core.Entities;
+using PMS.Core.Repository;
+using PMS.Infrastructure.Data.Context;
+
+namespace PMS.Infrastructure.Repository
+{
+    public class PropertyRepository : IPropertyRepository
+    {
+        private readonly PmsDbContext _context;
+
+
+
+        public PropertyRepository(PmsDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public async Task<Property> GetByIdAsync(Guid propertyId)
+            => await Task.FromResult(await _context.Properties.FirstOrDefaultAsync(p => p.Id == propertyId));
+
+        public async Task<IEnumerable<Property>> GetAllAsync(Guid userId)
+            => await Task.FromResult(_context.Properties.Where(p => p.UserId == userId).AsEnumerable());
+
+        public async Task CreateAsync(Property property)
+        {
+            if (property == null) 
+                throw new ArgumentNullException(nameof(property), "Property cannot be null");
+
+            await _context.Properties.AddAsync(property);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Property property)
+        {
+            if (property == null)
+                throw new ArgumentNullException(nameof(property), "Property cannot be null");
+
+            _context.Properties.Update(property);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Property property)
+        {
+            if (property == null)
+                throw new ArgumentNullException(nameof(property), "Property cannot be null");
+
+            _context.Properties.Remove(property);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
